367cb9bd07ee0909d04b8be04dc1f728d7ab4382
Clinery1 <clinery8237@gmail.com>
2023-01-02 21:38:33 -0500

    Rewrite it all to be much better

--- examples/debug.rs
+++ examples/debug.rs
@@ -1,9 +1,9 @@
-use std::fs::read_to_string;
-use regex_parser::{
-    Patterns,
-    StrOrData,
-    syntax,
+use std::{
+    fs::read_to_string,
+    time::Instant,
+    num::ParseIntError,
 };
+use regex_parser2::*;
 
 
 #[derive(Debug)]
@@ -18,39 +18,112 @@ enum Stmt<'a> {
     },
     Block(Vec<Self>),
 }
+#[derive(Debug)]
+enum Error {
+    NumParse(String),
+    InvalidMatch,
+    Multiple(Vec<Self>),
+}
+impl From<RegexParserError> for Error {
+    fn from(_:RegexParserError)->Self {
+        Error::InvalidMatch
+    }
+}
+impl From<Vec<Self>> for Error {
+    fn from(errs:Vec<Self>)->Self {
+        Self::Multiple(errs)
+    }
+}
+impl From<ParseIntError> for Error {
+    fn from(err:ParseIntError)->Self {
+        Self::NumParse(err.to_string())
+    }
+}
 
 
 fn main() {
     let data=read_to_string("example").unwrap();
-    let pats=syntax!{
-        type Out=Stmt;
-        "START" => "file","EOF" => |mut items|items.remove(0).as_data().unwrap();
-        "file" => "file inner"* => |items|{
-            Stmt::Block(items.into_iter().filter_map(StrOrData::as_data).collect())
-        };
-        "file inner" => "stmt"|"NL"|"WS";
-        "stmt" => "NL?","stmt inner","NL|EOF" => |mut items|items.remove(1).as_data().unwrap();
-        "stmt inner" => "declare"|"assign";
-        "declare" => "KW let","WS","WORD","WS?","EQUAL","WS?","NUMBER" =>|items|{
-            let name=items[2].as_str().unwrap();
-            let num=items[6].as_str().unwrap().parse().unwrap();
-            return Stmt::Declare{name,num};
-        };
-        "assign" => "WORD","WS?","EQUAL","WS?","NUMBER" =>|items|{
-            let name=items[0].as_str().unwrap();
-            let num=items[4].as_str().unwrap().parse().unwrap();
-            return Stmt::Assign{name,num};
-        };
-        "NL|EOF" => "NL"|"EOF";
-        "WORD" => r"[a-zA-Z_][a-zA-Z0-9_]*";
-        "NUMBER" => r"[0-9]*";
-        "EQUAL" => "=";
-        "WS" => "[ \t\r]+";
-        "WS?" => "[ \t\r]*";
-        "NL" => "\n+";
-        "NL?" => "\n*";
-        "EOF" => r"\z";
-        "KW let" => "let";
-    };
-    println!("{:#?}",pats.parse(&data));
+    let parser_create_start=Instant::now();
+    let word_parser=Map::new(
+        r"[a-zA-Z_][a-zA-Z0-9_]*".into_parser(),
+        |name_res:Result<_,Error>|match name_res {
+            Ok(name)=>Ok(name[0].matched),
+            Err(e)=>Err(e),
+        },
+    );
+    let usize_parser=number_parser::<usize,Error>();
+    let equal_parser="=".into_parser();
+    let ws_parser="[ \t\r]+".into_parser();
+    let opt_ws_parser="[ \t\r]*".into_parser();
+    let nl_parser="\n+".into_parser();
+    let eof_parser=r"\z".into_parser();
+    let let_kw_parser="let".into_parser();
+    let opt_nl_or_ws_parser="[ \r\t\n]*".into_parser();
+    let nl_or_eof_parser=(
+        &nl_parser,
+        &eof_parser,
+    )
+        .any()
+        .map(|res|match res {
+            Ok(o)=>Ok(o),
+            Err(mut e)=>Err(e.remove(0)),
+        });
+    let assign_parser=(
+        &word_parser,
+        &opt_ws_parser,
+        &equal_parser,
+        &opt_ws_parser,
+        &usize_parser,
+    ).map(|res|match res {
+        Ok((name,_,_,_,num))=>Ok(Stmt::Assign{name,num}),
+        Err(e)=>Err(e),
+    });
+    let declare_parser=(
+        &let_kw_parser,
+        &ws_parser,
+        &word_parser,
+        &opt_ws_parser,
+        &equal_parser,
+        &opt_ws_parser,
+        &usize_parser,
+    ).map(|res|match res {
+        Ok((_,_,name,_,_,_,num))=>Ok(Stmt::Declare{name,num}),
+        Err(e)=>Err(e),
+    });
+    let stmt_inner_parser=(
+        &declare_parser,
+        &assign_parser,
+    )
+        .any()
+        .map(|res|match res {
+            Ok(o)=>Ok(o),
+            Err(e)=>Err(Error::Multiple(e)),
+        });
+    let stmt_parser=(
+        &opt_nl_or_ws_parser,
+        stmt_inner_parser,
+        &nl_or_eof_parser,
+    ).map(|res|match res {
+        Ok((_,stmt,_))=>Ok(stmt),
+        Err(e)=>Err(e),
+    });
+    let file_inner_parser=stmt_parser.repeated(0).map(|res|match res {
+        Ok(stmts)=>Ok(Stmt::Block(stmts)),
+        Err(e)=>Err(e),
+    });
+    let file_parser=(
+        r"\A".into_parser(),
+        file_inner_parser,
+        r"\z".into_parser(),
+    ).map(|res|match res {
+        Ok((_,f,_))=>Ok(f),
+        Err(e)=>Err(e),
+    });
+    let parser_create_end=parser_create_start.elapsed();
+    println!("Took {:?} to create the parser",parser_create_end);
+    let mut buffer=Buffer::new(&data);
+    let start=Instant::now();
+    dbg!(file_parser.parse(&mut buffer)).ok();
+    let end=start.elapsed();
+    println!("Parsed in {:?}",end);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,234 +1,412 @@
-// use regex_syntax::ast::{
-//     // parse::Parser,
-//     // Ast,
-//     Error as RegexError,
-// };
+use num_traits::Bounded;
 use regex::{
     Regex,
+    Captures,
     Error as RegexError,
 };
 use std::{
-    fmt::{
-        Debug,
-        // Formatter,
-        // Result as FmtResult,
+    rc::{
+        Rc,
+        Weak,
     },
-    collections::HashMap,
+    str::FromStr,
+    marker::PhantomData,
+    borrow::Borrow,
 };
 
 
-#[macro_export]
-macro_rules! syntax {
-    {type Out=$t:ty;$($other:tt)*}=>{
-        {
-            let mut pats=$crate::Patterns::<$t>::new();
-            $crate::syntax_inner!(pats,$($other)*);
-            pats
+macro_rules! impl_tuple_parser {
+    ($first_par_ty:ident$(,$par_ty:ident)*)=>{
+        impl_tuple_parser!($first_par_ty,$($par_ty,)*);
+    };
+    ($($par_ty:ident,)+)=>{
+        impl<'a,$($par_ty:Parser<'a,ERR>,)+ERR> Parser<'a,ERR> for ($($par_ty,)+) {
+            type Output=($($par_ty::Output,)+);
+            #[allow(non_snake_case)]
+            fn parse(&self,buf:&mut Buffer<'a>)->Result<Self::Output,ERR> {
+                let ($($par_ty,)+)=self;
+                $(
+                    let buf_start=buf.index;
+                    let $par_ty=match $par_ty.parse(buf) {
+                        Ok(t)=>t,
+                        Err(e)=>{
+                            buf.index=buf_start;
+                            return Err(e.into());
+                        },
+                    };
+                )+
+                return Ok(($($par_ty,)+));
+            }
         }
     };
 }
-#[macro_export]
-macro_rules! syntax_inner {
-    ($pats:expr,$name:literal=>$first:literal$(|$extra:literal)+;$($end:tt)*)=>{
-        $pats.choice($name,[$first $(,$extra)+]);
-        $crate::syntax_inner!($pats,$($end)*);
+macro_rules! impl_tuple_parser_ext3 {
+    ($first_par_ty:ident$(,$par_ty:ident)*)=>{
+        impl_tuple_parser_ext3!($first_par_ty,$($par_ty,)*);
     };
-    ($pats:expr,$name:literal=>$first:literal$(,$extra:literal)* => $fold:expr;$($end:tt)*)=>{
-        $pats.sequence($name,[$first $(,$extra)*],&$fold);
-        $crate::syntax_inner!($pats,$($end)*);
+    ($($par_ty:ident,)+)=>{
+        impl<'a,'b,OUT,$($par_ty:Parser<'a,ERR,Output=OUT>+'b,)+ERR> ParserExt3<'a,'b,ERR,OUT> for ($(&'b $par_ty,)+) {
+            #[allow(non_snake_case)]
+            fn any(self)->Choice<'a,'b,OUT,ERR> {
+                let ($($par_ty,)+)=self;
+                let group=[$(
+                    $par_ty as &dyn Parser<'a,ERR,Output=OUT>,
+                )+];
+                return Choice::new(group);
+            }
+        }
     };
-    ($pats:expr,$name:literal=>$first:literal* => $fold:expr;$($end:tt)*)=>{
-        $pats.repeat($name,$first,&$fold);
-        $crate::syntax_inner!($pats,$($end)*);
-    };
-    ($pats:expr,$name:literal=>$first:literal+ => $fold:expr;$($end:tt)*)=>{
-        $pats.repeat1($name,$first,&$fold);
-        $crate::syntax_inner!($pats,$($end)*);
-    };
-    ($pats:expr,$name:literal=>$regex:literal;$($end:tt)*)=>{
-        $pats.terminal($name,$regex).unwrap();
-        $crate::syntax_inner!($pats,$($end)*);
-    };
-    ($pats:expr,)=>{};
 }
 
 
-pub type FoldFunction<'a,T>=&'a dyn Fn(Vec<StrOrData<'a,T>>)->T;
+pub trait Parser<'a,E> {
+    type Output;
+    /// It is up to the implementer to make sure that `buf` is left in a reusable state upon error.
+    fn parse(&self,buf:&mut Buffer<'a>)->Result<Self::Output,E>;
+}
+impl<'a,E,P:Parser<'a,E>> Parser<'a,E> for Box<P> {
+    type Output=P::Output;
+    fn parse(&self,buf:&mut Buffer<'a>)->Result<Self::Output,E> {
+        return <Box<P> as Borrow<P>>::borrow(self).parse(buf);
+    }
+}
+impl<'a,E,P:Parser<'a,E>> Parser<'a,E> for Rc<P> {
+    type Output=P::Output;
+    fn parse(&self,buf:&mut Buffer<'a>)->Result<Self::Output,E> {
+        return <Rc<P> as Borrow<P>>::borrow(self).parse(buf);
+    }
+}
+impl<'a,E,P:Parser<'a,E>> Parser<'a,E> for Weak<P> {
+    type Output=P::Output;
+    fn parse(&self,buf:&mut Buffer<'a>)->Result<Self::Output,E> {
+        let upgrade=self.upgrade().expect("Could not upgrade Weak<Parser> to Rc<Parser>");
+        return <Rc<P> as Borrow<P>>::borrow(&upgrade).parse(buf);
+    }
+}
+impl<'a,E,P:Parser<'a,E>> Parser<'a,E> for &P {
+    type Output=P::Output;
+    fn parse(&self,buf:&mut Buffer<'a>)->Result<Self::Output,E> {
+        (*self).parse(buf)
+    }
+}
+impl<'a,E,P:Parser<'a,E>> Parser<'a,E> for &mut P {
+    type Output=P::Output;
+    fn parse(&self,buf:&mut Buffer<'a>)->Result<Self::Output,E> {
+        (**self).parse(buf)
+    }
+}
+impl_tuple_parser!(A,B);
+impl_tuple_parser!(A,B,C);
+impl_tuple_parser!(A,B,C,D);
+impl_tuple_parser!(A,B,C,D,E);
+impl_tuple_parser!(A,B,C,D,E,F);
+impl_tuple_parser!(A,B,C,D,E,F,G);
+impl_tuple_parser!(A,B,C,D,E,F,G,H);
+impl_tuple_parser!(A,B,C,D,E,F,G,H,I);
+impl_tuple_parser!(A,B,C,D,E,F,G,H,I,J);
+
+pub trait ParserExt<'a,'b,E>:Parser<'a,E>+Sized+'b {
+    fn ignored(self)->Ignore<'a,E,Self> {
+        Ignore::new(self)
+    }
+    fn map<T,E2,F:Fn(Result<Self::Output,E>)->Result<T,E2>>(self,closure:F)->Map<'a,T,E2,E,Self,F> {
+        Map::new(self,closure)
+    }
+    fn boxed(self)->Box<dyn Parser<'a,E,Output=Self::Output>+'b> {
+        let boxed=Box::new(self);
+        return boxed;
+    }
+    fn repeated(self,times:usize)->Repeat<'a,Self::Output,E,Self> {
+        return Repeat::new(self,times);
+    }
+}
+impl<'a,'b,E,P:Parser<'a,E>+Sized+'b> ParserExt<'a,'b,E> for P {}
+
+pub trait ParserExt3<'a,'b,E,T>:'b {
+    fn any(self)->Choice<'a,'b,T,E>;
+}
+impl_tuple_parser_ext3!(A,B);
+impl_tuple_parser_ext3!(A,B,C);
+impl_tuple_parser_ext3!(A,B,C,D);
+impl_tuple_parser_ext3!(A,B,C,D,E);
+impl_tuple_parser_ext3!(A,B,C,D,E,F);
+impl_tuple_parser_ext3!(A,B,C,D,E,F,G);
+impl_tuple_parser_ext3!(A,B,C,D,E,F,G,H);
+impl_tuple_parser_ext3!(A,B,C,D,E,F,G,H,I);
+impl_tuple_parser_ext3!(A,B,C,D,E,F,G,H,I,J);
+
+pub trait IntoRegexParser<E> {
+    fn into_parser(&self)->RegexParser<E>;
+}
+impl<E> IntoRegexParser<E> for &str {
+    fn into_parser(&self)->RegexParser<E> {
+        RegexParser::new(self).expect("Invalid regex")
+    }
+}
 
 
-enum Pattern<'a,'source,T> {
-    Regex(Regex),
-    Sequence {
-        seq:Vec<&'a str>,
-        fold:FoldFunction<'source,T>,
-    },
-    Choice(Vec<&'a str>),
-    Repeat {
-        pat:&'a str,
-        fold:FoldFunction<'source,T>,
-    },
-    Repeat1 {
-        pat:&'a str,
-        fold:FoldFunction<'source,T>,
-    },
+enum RecursiveInner<T> {
+    Owned(Rc<T>),
+    Weak(Weak<T>),
 }
-// impl<'a,'source,T> Debug for Pattern<'a,'source,T> {
-//     fn fmt(&self,f:&mut Formatter)->FmtResult {
-//         match self {
-//             Self::Regex(raw,_,_)=>write!(f,"r{:?}",raw),
-//             Self::Sequence{seq,..}=>{
-//                 write!(f,"Sequence(")?;
-//                 seq.fmt(f)?;
-//                 write!(f,")")
-//             },
-//             Self::Choice(cs)=>{
-//                 write!(f,"Choice(")?;
-//                 cs.fmt(f)?;
-//                 write!(f,")")
-//             },
-//             _=>todo!(),
-//         }
-//     }
-// }
-impl<'a,'source,T> Pattern<'a,'source,T> {
-    pub fn sequence<L:IntoIterator<Item=&'a str>>(items:L,fold:FoldFunction<'source,T>)->Self {
-        return Self::Sequence{seq:items.into_iter().collect(),fold};
-    }
-    pub fn choice<L:IntoIterator<Item=&'a str>>(items:L)->Self {
-        return Self::Choice(items.into_iter().collect());
-    }
-    pub fn repeat(pat:&'a str,fold:FoldFunction<'source,T>)->Self {
-        return Self::Repeat{pat,fold};
-    }
-    pub fn repeat1(pat:&'a str,fold:FoldFunction<'source,T>)->Self {
-        return Self::Repeat1{pat,fold};
-    }
-}
-#[derive(Debug)]
-pub enum StrOrData<'a,T> {
-    Str(&'a str),
-    Strings(Vec<&'a str>),
-    Data(T),
-}
-impl<'a,T> StrOrData<'a,T> {
-    pub fn is_data(&self)->bool {
+impl<T> Clone for RecursiveInner<T> {
+    fn clone(&self)->Self {
         match self {
-            Self::Data(_)=>true,
-            _=>false,
+            Self::Owned(rc)=>Self::Owned(rc.clone()),
+            Self::Weak(weak)=>Self::Weak(weak.clone()),
         }
     }
-    pub fn as_str(&self)->Option<&'a str> {
+}
+impl<T> RecursiveInner<T> {
+    fn get_rc(&self)->Rc<T> {
         match self {
-            Self::Str(s)=>Some(s),
-            _=>None,
-        }
-    }
-    pub fn as_data(self)->Option<T> {
-        match self {
-            Self::Data(d)=>Some(d),
-            _=>None,
+            Self::Owned(rc)=>rc.clone(),
+            Self::Weak(weak)=>weak.upgrade().unwrap(),
         }
     }
 }
 
 
-pub struct Patterns<'a,'source,T> {
-    patterns:HashMap<&'a str,Pattern<'a,'source,T>>,
+pub struct Buffer<'a> {
+    raw:&'a str,
+    index:usize,
 }
-impl<'a,'source,T:Debug> Patterns<'a,'source,T> {
-    pub fn new()->Self {
-        Self {
-            patterns:HashMap::new(),
+impl<'a> Buffer<'a> {
+    pub fn new(raw:&'a str)->Self {
+        Buffer {
+            raw,
+            index:0,
         }
     }
-    pub fn terminal(&mut self,name:&'a str,regex:&'a str)->Result<(),RegexError> {
-        let regex=format!("\\A{}",regex);
-        // let ast=Parser::new().parse(&regex)?;
-        // if it isn't valid, then skip the expensive regex compile
-        let r=Regex::new(&regex).unwrap();
-        self.patterns.insert(name,Pattern::Regex(r));
-        return Ok(());
+    pub fn remaining(&self)->&'a str {
+        &self.raw[self.index..]
     }
-    pub fn sequence<L:IntoIterator<Item=&'a str>>(&mut self,name:&'a str,patterns:L,fold:FoldFunction<'source,T>) {
-        let seq=Pattern::sequence(patterns,fold);
-        self.patterns.insert(name,seq);
+    pub fn add_offset(&mut self,to_add:usize) {
+        self.index+=to_add;
     }
-    pub fn choice<L:IntoIterator<Item=&'a str>>(&mut self,name:&'a str,patterns:L) {
-        self.patterns.insert(name,Pattern::choice(patterns));
+    pub fn try_match_regex(&self,regex:&Regex)->Option<Captures> {
+        return regex.captures(self.remaining());
     }
-    pub fn repeat(&mut self,name:&'a str,pat:&'a str,fold:FoldFunction<'source,T>) {
-        let seq=Pattern::repeat(pat,fold);
-        self.patterns.insert(name,seq);
+    pub fn index(&self)->usize {self.index}
+    pub fn set_index(&mut self,index:usize) {
+        self.index=index;
     }
-    pub fn repeat1(&mut self,name:&'a str,pat:&'a str,fold:FoldFunction<'source,T>) {
-        let seq=Pattern::repeat1(pat,fold);
-        self.patterns.insert(name,seq);
-    }
-    pub fn parse(&self,source:&'source str)->Option<StrOrData<'source,T>> {
-        if let Some((_,d))=self.inner_parse("START",source) {
-            return Some(d);
-        }
-        return None;
-    }
-    fn inner_parse(&self,name:&'a str,source:&'source str)->Option<(usize,StrOrData<'source,T>)> {
-        // println!("Pattern `{}`",name);
-        let pat=self.patterns.get(name).unwrap();
-        match pat {
-            Pattern::Regex(r)=>{
-                let mut v=Vec::new();
-                let caps=r.captures(source)?;
-                let mut len=0;
-                for (i,cap) in caps.iter().enumerate() {
-                    if i==0 {
-                        len=cap?.end();
-                    }
-                    v.push(cap?.as_str());
-                }
-                if v.len()==1 {
-                    return Some((len,StrOrData::Str(v.remove(0))));
-                }
-                return Some((len,StrOrData::Strings(v)));
-            },
-            Pattern::Sequence{seq,fold}=>{
-                let mut stack=Vec::new();
-                let mut len=0;
-                for name in seq.iter() {
-                    let (add_len,item)=self.inner_parse(name,&source[len..])?;
-                    len+=add_len;
-                    stack.push(item);
-                }
-                return Some((len,StrOrData::Data(fold(stack))));
-            },
-            Pattern::Choice(choices)=>{
-                for choice in choices {
-                    let ret=self.inner_parse(choice,source);
-                    if ret.is_some() {
-                        return ret;
-                    }
-                }
-                return None;
-            },
-            Pattern::Repeat{pat,fold}=>{
-                let mut stack=Vec::new();
-                let mut len=0;
-                while let Some((add_len,item))=self.inner_parse(pat,&source[len..]) {
-                    stack.push(item);
-                    len+=add_len;
-                }
-                return Some((len,StrOrData::Data(fold(stack))));
-            },
-            Pattern::Repeat1{pat,fold}=>{
-                let mut stack=Vec::new();
-                let mut len=0;
-                while let Some((add_len,item))=self.inner_parse(pat,&source[len..]) {
-                    stack.push(item);
-                    len+=add_len;
-                }
-                if stack.len()==0 {
-                    return None;
-                }
-                return Some((len,StrOrData::Data(fold(stack))));
-            },
+}
+
+pub struct RegexParserError;
+impl From<RegexParserError> for () {fn from(_:RegexParserError) {()}}
+
+#[derive(Debug,Copy,Clone)]
+pub struct RegexParserMatch<'a> {
+    pub start:usize,
+    pub end:usize,
+    pub matched:&'a str,
+}
+
+/// Matches a regex. Results in a list of `RegexParserMatch`s with the first being the entire match
+/// and each subsequent match being a capture group
+pub struct RegexParser<E> {
+    regex:Regex,
+    _phantom:PhantomData<E>,
+}
+impl<E> Clone for RegexParser<E> {
+    fn clone(&self)->Self {
+        RegexParser {
+            regex:self.regex.clone(),
+            _phantom:PhantomData,
         }
     }
+}
+impl<E> RegexParser<E> {
+    pub fn new(r:&str)->Result<Self,RegexError> {
+        let regex=Regex::new(r)?;
+        return Ok(RegexParser {
+            regex,
+            _phantom:PhantomData,
+        });
+    }
+    fn parse_inner<'a>(&self,buf:&mut Buffer<'a>)->Option<Vec<RegexParserMatch<'a>>> {
+        let mut out=Vec::new();
+        let buf_offset=buf.index();
+        let caps=self.regex.captures(buf.remaining())?;
+        let mut captures=caps.iter().peekable();
+        let first=captures.peek()?.expect("First regex capture should always exist");
+        for cap in &mut captures {
+            let cap=cap?;
+            let end=cap.end()+buf_offset;
+            let start=cap.start()+buf_offset;
+            out.push(RegexParserMatch{start,end,matched:cap.as_str()});
+        }
+        buf.add_offset(first.end());
+        return Some(out);
+    }
+}
+impl<'a,E:From<RegexParserError>> Parser<'a,E> for RegexParser<E> {
+    type Output=Vec<RegexParserMatch<'a>>;
+    fn parse(&self,buf:&mut Buffer<'a>)->Result<Self::Output,E> {
+        self.parse_inner(buf).ok_or(RegexParserError.into())
+    }
+}
+
+pub struct Recursive<'a,T,E,P:Parser<'a,E,Output=T>>{
+    inner:RecursiveInner<P>,
+    _phantom:PhantomData<&'a (T,E)>,
+}
+impl<'a,T,E,P:Parser<'a,E,Output=T>> Clone for Recursive<'a,T,E,P> {
+    fn clone(&self)->Self {
+        Recursive {
+            inner:self.inner.clone(),
+            _phantom:PhantomData,
+        }
+    }
+}
+impl<'a,T,E,P:Parser<'a,E,Output=T>> Recursive<'a,T,E,P> {
+    pub fn new<F:FnOnce(Self)->P>(closure:F)->Self {
+        return Recursive {
+            inner:RecursiveInner::Owned(Rc::new_cyclic(|weak|{
+                let rec=Recursive {
+                    inner:RecursiveInner::Weak(weak.clone()),
+                    _phantom:PhantomData,
+                };
+                closure(rec)
+            })),
+            _phantom:PhantomData,
+        };
+    }
+}
+impl<'a,T,E,P:Parser<'a,E,Output=T>> Parser<'a,E> for Recursive<'a,T,E,P> {
+    type Output=T;
+    fn parse(&self,buf:&mut Buffer<'a>)->Result<T,E> {
+        return <Rc<P> as Borrow<P>>::borrow(&self.inner.get_rc()).parse(buf);
+    }
+}
+
+pub struct Choice<'a,'b,T,E> {
+    choices:Vec<&'b (dyn Parser<'a,E,Output=T>+'b)>,
+}
+impl<'a,'b,T,E> Choice<'a,'b,T,E> {
+    pub fn new<I:IntoIterator<Item=&'b (dyn Parser<'a,E,Output=T>+'b)>>(parsers:I)->Self {
+        Choice {
+            choices:parsers.into_iter().collect(),
+        }
+    }
+}
+impl<'a,'b,T,E> Parser<'a,Vec<E>> for Choice<'a,'b,T,E> {
+    type Output=T;
+    fn parse(&self,buf:&mut Buffer<'a>)->Result<T,Vec<E>> {
+        let buf_index_start=buf.index;
+        let mut errs=Vec::new();
+        for choice in self.choices.iter() {
+            match choice.parse(buf) {
+                Ok(res)=>{
+                    return Ok(res);
+                },
+                Err(e)=>{
+                    errs.push(e);
+                    buf.index=buf_index_start;
+                },
+            }
+        }
+        return Err(errs);
+    }
+}
+
+pub struct Repeat<'a,T,E,P:Parser<'a,E,Output=T>> {
+    inner:P,
+    min_count:usize,
+    _phantom:PhantomData<&'a (T,E)>,
+}
+impl<'a,T,E,P:Parser<'a,E,Output=T>> Repeat<'a,T,E,P> {
+    pub fn new(parser:P,min_count:usize)->Self {
+        Repeat {
+            inner:parser,
+            min_count,
+            _phantom:PhantomData,
+        }
+    }
+}
+impl<'a,T,E,P:Parser<'a,E,Output=T>> Parser<'a,E> for Repeat<'a,T,E,P> {
+    type Output=Vec<T>;
+    fn parse(&self,buf:&mut Buffer<'a>)->Result<Vec<T>,E> {
+        let mut out=Vec::new();
+        let buf_start_outer=buf.index;
+        loop {
+            let buf_start=buf.index;
+            match self.inner.parse(buf) {
+                Ok(t)=>out.push(t),
+                Err(e)=>if out.len()<self.min_count {
+                    buf.index=buf_start_outer;
+                    return Err(e);
+                } else {
+                    buf.index=buf_start;
+                    break;
+                },
+            }
+        }
+        return Ok(out);
+    }
+}
+
+pub struct Map<'a,T,E,E2,P:Parser<'a,E2>,F:Fn(Result<P::Output,E2>)->Result<T,E>> {
+    prev:P,
+    closure:F,
+    _phantom:PhantomData<&'a (T,E,E2)>,
+}
+impl<'a,T,E,E2,P:Parser<'a,E2>,F:Fn(Result<P::Output,E2>)->Result<T,E>> Map<'a,T,E,E2,P,F> {
+    pub fn new(prev:P,closure:F)->Self {
+        Map {
+            prev,
+            closure,
+            _phantom:PhantomData,
+        }
+    }
+}
+impl<'a,T,E,E2,P:Parser<'a,E2>,F:Fn(Result<P::Output,E2>)->Result<T,E>> Parser<'a,E> for Map<'a,T,E,E2,P,F> {
+    type Output=T;
+    fn parse(&self,buf:&mut Buffer<'a>)->Result<T,E> {
+        let buf_start=buf.index;
+        let out=self.prev.parse(buf);
+        if out.is_err() {
+            buf.index=buf_start;
+        }
+        return (self.closure)(out);
+    }
+}
+
+pub struct Ignore<'a,E,P:Parser<'a,E>> {
+    inner:P,
+    _phantom:PhantomData<&'a E>,
+}
+impl<'a,E,P:Parser<'a,E>> Ignore<'a,E,P> {
+    pub fn new(parser:P)->Self {
+        Ignore {
+            inner:parser,
+            _phantom:PhantomData,
+        }
+    }
+}
+impl<'a,E,P:Parser<'a,E>> Parser<'a,E> for Ignore<'a,E,P> {
+    type Output=();
+    fn parse(&self,buf:&mut Buffer<'a>)->Result<(),E> {
+        match self.inner.parse(buf) {
+            Ok(_)=>Ok(()),
+            Err(e)=>return Err(e),
+        }
+    }
+}
+
+
+pub fn number_parser<'a,NUM:Bounded+FromStr+'a,E:From<RegexParserError>+From<<NUM as FromStr>::Err>+'a>()->impl Parser<'a,E,Output=NUM> {
+    return Map::new(
+        RegexParser::new(r"[0-9]*").unwrap(),
+        |number_res:Result<_,E>|match number_res {
+            Ok(matches)=>{
+                match matches[0].matched.parse::<NUM>() {
+                    Ok(val)=>Ok(val),
+                    Err(e)=>Err(e.into()),
+                }
+            },
+            Err(e)=>Err(e.into()),
+        },
+    );
 }
